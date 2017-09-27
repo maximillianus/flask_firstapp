63b834b6a68bbbb2aaaa7e712f2b6c691c30bbc3
Pradana <pradana1@global.anz.com>
2017-09-27 17:56:08 +0800

    Checkbox input, input validation commit. modified hello.py with input() function. \nAdded input jinja2 html file.

--- hello.py
+++ hello.py
@@ -7,6 +7,16 @@ def hello_world():
 	return render_template('index.html.j2', user_agent=user_agent)
 	#return "<p>Your Browser is: %s</p>" % user_agent
 
+@app.route('/input/', methods=['GET','POST'])
+def input():
+	if request.method == 'POST':
+		passedvar = request.form.getlist('optionchkbox')
+		return render_template('input.html.j2', passedvar = passedvar)
+	elif request.args.get('option'):
+		return("GET: " + request.args.get('option'))
+	return render_template('input.html.j2')
+
+
 ## If running with 'python hello.py' command
 # if __name__ == '__main__':
 # 	app.run()
